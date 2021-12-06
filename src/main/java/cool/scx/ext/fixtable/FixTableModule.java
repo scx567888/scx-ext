c8a7ebf81ff8bad6b01bc41932ce2e1b7a9b49d6
scx567888 <scx567888@outlook.com>
2021-12-06 22:10:38 +0800

    Dev (#15)
    * 1,更新依赖项
    
    * 1.5.11

--- src/main/java/cool/scx/ext/fixtable/FixTableModule.java
+++ src/main/java/cool/scx/ext/fixtable/FixTableModule.java
@@ -3,6 +3,8 @@ package cool.scx.ext.fixtable;
 import cool.scx.ScxContext;
 import cool.scx.ScxModule;
 import cool.scx.dao.ScxDaoHelper;
+import cool.scx.util.ConsoleUtils;
+import org.slf4j.LoggerFactory;
 
 /**
  * <p>FixTableModule class.</p>
@@ -13,11 +15,46 @@ import cool.scx.dao.ScxDaoHelper;
 public class FixTableModule implements ScxModule {
 
     /**
+     * a
+     *
+     * @return a
+     */
+    private static boolean confirmFixTable() {
+        while (true) {
+            System.err.println("Y 检测到需要修复数据表 , 是否修复? [Y]修复数据表 [N]忽略 [Q]退出 ");
+            var result = ConsoleUtils.readLine().trim();
+            if ("Y".equalsIgnoreCase(result)) {
+                return true;
+            } else if ("N".equalsIgnoreCase(result)) {
+                return false;
+            } else if ("Q".equalsIgnoreCase(result)) {
+                System.exit(-1);
+                return false;
+            }
+        }
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Override
     public void start() {
-        ScxContext.execute(() -> ScxDaoHelper.fixAllTable(false));
+        var logger = LoggerFactory.getLogger(FixTableModule.class);
+        ScxContext.execute(() -> {
+            if (!ScxContext.dao().checkDataSource()) {
+                logger.error("数据源连接失败!!! 已跳过修复表!!!");
+                return;
+            }
+            if (ScxDaoHelper.checkNeedFixTable()) {
+                if (confirmFixTable()) {
+                    ScxDaoHelper.fixTable();
+                } else {
+                    logger.debug("用户已取消修复表 !!!");
+                }
+            } else {
+                logger.debug("没有表需要修复...");
+            }
+        });
     }
 
     /**
