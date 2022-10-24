9f877a24c80965da44a294292bf959fdb409ab7f
scx567888 <scx567888@outlook.com>
2022-10-24 17:21:12 +0800

    重新设计 FSSModule close #186

--- src/main/java/cool/scx/ext/fss/FSSObjectService.java
+++ src/main/java/cool/scx/ext/fss/FSSObjectService.java
@@ -3,7 +3,11 @@ package cool.scx.ext.fss;
 import cool.scx.core.annotation.ScxService;
 import cool.scx.core.base.BaseModelService;
 import cool.scx.sql.base.Query;
+import cool.scx.util.FileUtils;
 
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 import java.util.List;
 
 /**
@@ -14,6 +18,16 @@ import java.util.List;
  */
 @ScxService
 public class FSSObjectService extends BaseModelService<FSSObject> {
+
+    /**
+     * 获取物理文件路径
+     *
+     * @param fssObject a {@link cool.scx.ext.fss.FSSObject} object
+     * @return a {@link java.nio.file.Path} object
+     */
+    public static Path getPhysicalFilePath(FSSObject fssObject) {
+        return Path.of(FSSConfig.uploadFilePath().toString(), fssObject.filePath);
+    }
 
     /**
      * 根据 md5 查找文件
@@ -53,6 +67,32 @@ public class FSSObjectService extends BaseModelService<FSSObject> {
      */
     public List<FSSObject> findByFSSObjectIDs(List<String> fssObjectIDs) {
         return list(new Query().in("fssObjectID", fssObjectIDs));
+    }
+
+    /**
+     * 根据 fssObjectID 进行删除 (同时还会删除物理文件, 如果引用为 0 的话)
+     *
+     * @param fssObjectID f
+     * @throws java.io.IOException f
+     */
+    public void delete(String fssObjectID) throws IOException {
+        //先获取文件的基本信息
+        var needDeleteFile = this.findByFSSObjectID(fssObjectID);
+        if (needDeleteFile != null) {
+            //判断文件是否被其他人引用过
+            long count = this.countByMD5(needDeleteFile.fileMD5);
+            //没有被其他人引用过 可以删除物理文件
+            if (count <= 1) {
+                var filePath = getPhysicalFilePath(needDeleteFile);
+                try {
+                    FileUtils.delete(filePath.getParent());
+                } catch (NoSuchFileException ignore) {
+                    //文件不存在时忽略错误
+                }
+            }
+            //删除数据库中的文件数据
+            this.delete(needDeleteFile.id);
+        }
     }
 
 }
