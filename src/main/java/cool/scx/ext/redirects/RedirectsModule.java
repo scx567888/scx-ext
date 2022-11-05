c9bb4757f7ea2391e93be1835ce4d3b39c74664f
scx567888 <scx567888@outlook.com>
2022-11-05 12:25:32 +0800

    1.15.3

--- src/main/java/cool/scx/ext/redirects/RedirectsModule.java
+++ src/main/java/cool/scx/ext/redirects/RedirectsModule.java
@@ -4,6 +4,7 @@ import cool.scx.core.ScxContext;
 import cool.scx.core.ScxModule;
 import cool.scx.core.vo.Redirections;
 import cool.scx.util.ansi.Ansi;
+import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
 
 /**
@@ -16,35 +17,52 @@ public class RedirectsModule extends ScxModule {
 
     private final int port;
 
+    /**
+     * <p>Constructor for RedirectsModule.</p>
+     */
     public RedirectsModule() {
         this(80);
     }
 
+    /**
+     * <p>Constructor for RedirectsModule.</p>
+     *
+     * @param port a int
+     */
     public RedirectsModule(int port) {
         this.port = port;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String name() {
         return "SCX_EXT-" + super.name();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void start() {
         //只有当开启 https 的时候才进行转发
         if (ScxContext.coreConfig().isHttpsEnabled()) {
-            init();
+            startRedirects(ScxContext.vertx(), this.port);
         }
     }
 
-    private void init() {
-        var router = Router.router(ScxContext.vertx());
+    /**
+     * 也可以直接以工具类的形式调用
+     */
+    public static void startRedirects(Vertx vertx, int port) {
+        var router = Router.router(vertx);
         router.route().handler(c -> {
             var oldURI = c.request().absoluteURI();
             var newURI = "https" + oldURI.substring("http".length());
             Redirections.ofTemporary(newURI).handle(c);
         });
-        ScxContext.vertx().createHttpServer().requestHandler(router).listen(port, (http) -> {
+        vertx.createHttpServer().requestHandler(router).listen(port, (http) -> {
             if (http.succeeded()) {
                 Ansi.out().brightMagenta("转发服务器启动成功 http -> https, 端口号 : " + port + " !!!").println();
             } else {
