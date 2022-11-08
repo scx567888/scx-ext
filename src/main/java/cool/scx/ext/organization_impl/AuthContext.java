86a1f136a0515018cc58870839c981ddc90cf3ee
scx567888 <scx567888@outlook.com>
2022-11-08 14:28:01 +0800

    1.15.4

--- src/main/java/cool/scx/ext/organization_impl/AuthContext.java
+++ src/main/java/cool/scx/ext/organization_impl/AuthContext.java
@@ -1,5 +1,6 @@
 package cool.scx.ext.organization_impl;
 
+import cool.scx.ext.organization.auth.PermFlag;
 import cool.scx.ext.organization_impl.impl.AuthHandler;
 import cool.scx.ext.organization_impl.impl.User;
 
@@ -9,6 +10,14 @@ public class AuthContext {
 
     public static User getCurrentUser() {
         return authHandler.getCurrentUser();
+    }
+
+    public static boolean hasPerm(String permString) {
+        return authHandler.hasPerm(permString);
+    }
+
+    public static boolean hasPerm(PermFlag permFlag) {
+        return authHandler.hasPerm(permFlag);
     }
 
     static void setAuthHandler(AuthHandler handler) {
