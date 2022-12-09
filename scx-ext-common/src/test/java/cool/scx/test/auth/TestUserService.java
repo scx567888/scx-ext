2c3ba69a10154e09068abad335611dc8fc8223d4
scx567888 <scx567888@outlook.com>
2022-12-09 21:12:16 +0800

    dev

--- scx-ext-common/src/test/java/cool/scx/test/auth/TestUserService.java
+++
@@ -1,9 +0,0 @@
-package cool.scx.test.auth;
-
-import cool.scx.core.annotation.ScxService;
-import cool.scx.core.base.BaseModelService;
-
-@ScxService
-public class TestUserService extends BaseModelService<TestUser> {
-
-}
