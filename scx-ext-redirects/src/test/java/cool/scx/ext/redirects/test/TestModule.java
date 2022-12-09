825a4d06ced06bb1bc96936ec43d9ad7f639e4da
scx567888 <scx567888@outlook.com>
2022-12-09 20:32:13 +0800

    dev

---
+++ scx-ext-redirects/src/test/java/cool/scx/ext/redirects/test/TestModule.java
@@ -0,0 +1,56 @@
+package cool.scx.ext.redirects.test;
+
+import cool.scx.core.Scx;
+import cool.scx.core.ScxModule;
+import cool.scx.core.enumeration.ScxCoreFeature;
+import cool.scx.ext.redirects.RedirectsModule;
+import cool.scx.util.http.HttpClientHelper;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * <p>TestModule class.</p>
+ *
+ * @author scx567888
+ * @version 1.3.14
+ * @since 1.3.14
+ */
+public class TestModule extends ScxModule {
+
+    /**
+     * <p>main.</p>
+     *
+     * @param args an array of {@link java.lang.String} objects
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        runModule();
+        test0();
+    }
+
+    @BeforeTest
+    public static void runModule() {
+        Scx.builder()
+                .setMainClass(TestModule.class)
+                .addModule(
+                        new TestModule(),
+                        new RedirectsModule())
+                .configure(ScxCoreFeature.USE_DEVELOPMENT_ERROR_PAGE, true)
+                .run();
+    }
+
+    @Test
+    public static void test0() throws IOException, InterruptedException {
+        HttpClientHelper.get("http://localhost:8080/");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String name() {
+        return "SCX_EXT-" + super.name();
+    }
+
+}
---
+++ scx-ext-ws/src/test/java/cool/scx/ext/ws/test/website/WriteTimeHandler.java
@@ -0,0 +1,34 @@
+package cool.scx.ext.ws.test.website;
+
+import cool.scx.core.ScxConstant;
+import cool.scx.core.ScxContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static cool.scx.ext.ws.WSContext.wsConsumer;
+import static cool.scx.ext.ws.WSContext.wsPublishAll;
+
+public class WriteTimeHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(WriteTimeHandler.class);
+
+    /**
+     * 注册所有的 handler 请在模块 start 中调用
+     */
+    public static void registerHandler() {
+        //注册事件
+        ScxContext.scheduler().scheduleAtFixedRate((status) -> {
+            var nowTime = ScxConstant.DEFAULT_DATETIME_FORMATTER.format(LocalDateTime.now());
+            wsPublishAll("writeTime", nowTime);
+        }, Duration.ofSeconds(1));
+        //注册一个监控前台浏览器宽高变化的 handler
+        wsConsumer("onResizeBrowser", (wsParam) -> {
+            //测试
+            logger.error("浏览器大小 : " + wsParam.body());
+        });
+    }
+
+}
