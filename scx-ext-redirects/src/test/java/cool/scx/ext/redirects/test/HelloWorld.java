26f56c774d39e1b6a5e20d5b3a1c2c87432d7513
scx567888 <scx567888@outlook.com>
2024-01-23 11:25:19 +0800

    dev

---
+++ scx-ext-redirects/src/test/java/cool/scx/ext/redirects/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package cool.scx.ext.redirects.test;
+
+import cool.scx.mvc.annotation.ScxRoute;
+
+@ScxRoute
+public class HelloWorld {
+
+    @ScxRoute("")
+    public String helloWorld() {
+        return "Hello World";
+    }
+    
+}
